fe8729484e54bb3d619daf867329ea1784bfad05
Trey Piepmeier <trey@treypiepmeier.com>
2019-10-03 15:37:09 -0400

    Initial commit

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "stylelint-config-standard",
+    "rules": {
+        "indentation": 4,
+        "string-quotes": "single",
+        "at-rule-no-unknown": null,
+    },
+    "ignoreFiles": "src/_includes/scss/partials/vendor/**",
+}
