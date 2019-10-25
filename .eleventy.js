0c40685142583c2a13236358ce6756f327823f2c
Trey Piepmeier <trey@treypiepmeier.com>
2019-10-25 15:26:08 -0400

    A little progressively enhanced niceness
    May be crude, but it’s better than it was. Pretty much just took this
    from my circa-2012 version of this site.

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,8 @@ const md = new MarkdownIt();
 module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter('markdown', value => md.renderInline(value));
 
+    eleventyConfig.addPassthroughCopy('src/js');
+
     return {
         dir: {
             input: 'src',
