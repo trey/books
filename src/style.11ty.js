79246f3051cb998599a575a26e0360b0a85d5b75
Trey Piepmeier <trey@treypiepmeier.com>
2024-10-30 15:00:22 -0400

    Get rid of Sass deprecation warnings.

--- src/style.11ty.js
+++ src/style.11ty.js
@@ -1,7 +1,5 @@
 // https://gist.github.com/liamfiddler/07e2878755a0a631a584b6420866424e
-const util = require('util');
 const sass = require('sass'); // `npm i -D sass`
-const renderSass = util.promisify(sass.render);
 const inputFile = 'src/_includes/scss/main.scss'; // the path to your main SCSS file
 const outputFile = 'css/main.css'; // the filename you want this template to be saved as
 
@@ -13,9 +11,7 @@ module.exports = class {
         };
     }
     async render() {
-        const result = await renderSass({
-            file: inputFile,
-        });
+        const result = await sass.compileAsync(inputFile);
 
         return result.css;
     }
